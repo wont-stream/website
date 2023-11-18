3721d56016ea2e0efc1b84a860f0354020a7e20d
Katze <git@katze.click>
2023-11-18 16:09:01 -0500

    add files that ddint get commited?

---
+++ public/assets/scripts/lanyard.js
@@ -0,0 +1,50 @@
+const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
+const avatar = document.getElementById("avatar");
+
+lanyard.onmessage = ({ data }) => {
+    data = JSON.parse(data);
+
+    switch (data.op) {
+        case 0: {
+            switch (data.t) {
+                case "INIT_STATE": {
+                    discord(data.d["1125315673829154837"])
+                }
+                break
+
+                case "PRESENCE_UPDATE": {
+                    discord(data.d)
+                }
+                break
+
+                default: {}
+                break
+            }
+        };
+        break
+
+        case 1: {
+            lanyard.send(JSON.stringify({
+                op: 2,
+                d: {
+                  subscribe_to_ids: ["1125315673829154837"]
+                }
+              }))
+
+              setInterval((() => {
+                lanyard.send(JSON.stringify({
+                    op: 3
+                  }))
+              }), data.d.heartbeat_interval)
+              
+        };
+        break
+
+        default: {};
+        break
+    }
+};
+
+const discord = (data) => {
+    avatar.classList = `avatar ${data.discord_status}`
+}
