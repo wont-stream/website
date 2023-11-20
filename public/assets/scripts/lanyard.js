52f8307f655c35a1e525f55db1712e4507fabf6d
Katze <143244075+de-katze@users.noreply.github.com>
2023-11-20 11:00:47 -0500

    Update lanyard.js

--- public/assets/scripts/lanyard.js
+++ public/assets/scripts/lanyard.js
@@ -1,50 +1,13 @@
-const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
 const avatar = document.getElementById("avatar");
 
-lanyard.onmessage = ({ data }) => {
-    data = JSON.parse(data);
+const grabData = async () => {
+    let data = await fetch("https://lanyard.rest/v1/users/1125315673829154837")
 
-    switch (data.op) {
-        case 0: {
-            switch (data.t) {
-                case "INIT_STATE": {
-                    discord(data.d["1125315673829154837"])
-                }
-                break
-
-                case "PRESENCE_UPDATE": {
-                    discord(data.d)
-                }
-                break
-
-                default: {}
-                break
-            }
-        };
-        break
-
-        case 1: {
-            lanyard.send(JSON.stringify({
-                op: 2,
-                d: {
-                  subscribe_to_ids: ["1125315673829154837"]
-                }
-              }))
-
-              setInterval((() => {
-                lanyard.send(JSON.stringify({
-                    op: 3
-                  }))
-              }), data.d.heartbeat_interval)
-              
-        };
-        break
-
-        default: {};
-        break
-    }
-};
+    discord(await data.json())
+}
 
 const discord = (data) => {
     avatar.classList = `avatar ${data.discord_status}`
 }
+
+grabData()
