4e6292adc8a608442817de5bafd2b009602610af
Katze <git@katze.click>
2023-11-26 20:16:01 -0500

    Hop auto runs build

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const handler = require('serve-handler');
-let { exec } = require("child_process");
-const { promisify } = require('util');
 const http = require('http');
 
-exec = promisify(exec);
-
-(async () => {
-    await exec("pnpm i")
-
-    await exec("pnpm build")
-
-    http.createServer(async (req, res) => await handler(req, res, {
-        public: "public",
-        cleanUrls: true,
-        trailingSlash: false
-    })).listen(3000, () => {
-        console.log("Running at :3000")
-    });
-})()
+http.createServer(async (req, res) => await handler(req, res, {
+    public: "public",
+    cleanUrls: true,
+    trailingSlash: false
+})).listen(3000, () => {
+    console.log("Running at :3000")
+});
