0d7e1c2c40bdf636455880116f5853c25d480517
Katze <143244075+de-katze@users.noreply.github.com>
2023-12-09 16:40:55 -0500

    Update heartrate.js

--- public/assets/scripts/heartrate.js
+++ public/assets/scripts/heartrate.js
@@ -1,12 +1,10 @@
 const hyperheart = new WebSocket("wss://hyperheart.katze.click/");
 const hr = document.getElementById("heart");
 const avgHr = document.getElementById("avg-heart");
-const numbers = [];
 
 hyperheart.onmessage = ({ data }) => {
-    const heartRate = JSON.parse(data).hr;
-    numbers.push(heartRate);
+    const {hr, avg} = JSON.parse(data);
 
-    hr.style.setProperty("--value", heartRate)
-    avgHr.style.setProperty("--value", Math.floor(numbers.reduce((acc, num) => acc + num, 0) / numbers.length))
+    hr.style.setProperty("--value", hr)
+    avgHr.style.setProperty("--value", avg)
 };
